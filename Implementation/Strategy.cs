9f92e9a61d19e03d1d454d6b593b9e8be576449d
max.weber <max.weber@xempus.com>
2025-02-25 13:49:10 +0100

    Add strategy pattern

---
+++ Implementation/Strategy.cs
@@ -0,0 +1,30 @@
+﻿namespace Implementation;
+
+public interface IShippingStrategy
+{
+    decimal CalculateShippingCost(decimal orderTotal);
+}
+
+public class StandardShipping : IShippingStrategy
+{
+    public decimal CalculateShippingCost(decimal orderTotal)
+    {
+        return 3 * orderTotal;
+    }
+}
+
+public class ExpressShipping : IShippingStrategy
+{
+    public decimal CalculateShippingCost(decimal orderTotal)
+    {
+        return 5 * orderTotal;
+    }
+}
+
+public class Strategy(IShippingStrategy strategy)
+{
+    public decimal GetShippingCost(decimal amount)
+    {
+        return strategy.CalculateShippingCost(amount);
+    }
+}
---
+++ Tests/StrategyTests.cs
@@ -0,0 +1,28 @@
+﻿using Implementation;
+
+namespace Tests;
+
+public class StrategyTests
+{
+    [TestCase(0, ExpectedResult = 0)]
+    [TestCase(1, ExpectedResult = 3)]
+    [TestCase(3, ExpectedResult = 9)]
+    public decimal GetShippingCostTest_StandardShipping(decimal amount)
+    {
+        var strategy = new StandardShipping();
+        var target = new Strategy(strategy);
+
+        return target.GetShippingCost(amount);
+    }
+
+    [TestCase(0, ExpectedResult = 0)]
+    [TestCase(1, ExpectedResult = 5)]
+    [TestCase(3, ExpectedResult = 15)]
+    public decimal GetShippingCostTest_ExpressShipping(decimal amount)
+    {
+        var strategy = new ExpressShipping();
+        var target = new Strategy(strategy);
+
+        return target.GetShippingCost(amount);
+    }
+}
